e8587661d4bdb7bc830b40cc48aedf6cfd4f0ac5
WIIN2602 <weeradech2602@gmail.com>
2023-12-19 20:41:13 +0700

    last update

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function showAlert() {
-    //show notification on window
-    alert('Button clicked! Hello, World!');
+function LearnMoreBTN() {
+    window.location.href="learnmore.html"
 }
-
-function submitForm() {
-    // Get the input value
-    var userName = document.getElementById("userName").value;
-
-    // Display the input value
-    document.getElementById("output").innerHTML = "Hello, " + userName + "!";
-}
-
-document.getElementById("Sign-in.button").addEventListener("click", function() {
-    window.location.href = "index.html";
-});
