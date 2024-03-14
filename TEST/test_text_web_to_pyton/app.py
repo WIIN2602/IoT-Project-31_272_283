13b10e13e52b6125d0093bc06512a29763bea63f
WIIN2602 <weeradech2602@gmail.com>
2024-03-14 21:05:01 +0700

    ????

--- TEST/test_text_web_to_pyton/app.py
+++ TEST/test_text_web_to_pyton/app.py
@@ -1,28 +1,11 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-
-# @app.route('/process_text', methods=['POST'])
-# def process_text():
-#     data = request.get_json()
-#     text = data.get('text', '')
-
-#     # Process the text (for example, you can print it)
-#     print("Received text:", text)
-
-#     # You can do some processing here and return a response
-#     processed_text = text.upper()  # Example: Convert text to uppercase
-#     return jsonify({'result': processed_text})
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)  # Run the Flask app
-
-
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/submit', methods=['POST'])
