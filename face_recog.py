f46cc22acd138d7d8c7e940b346f2e08c98d068c
WIIN2602 <weeradech2602@gmail.com>
2024-03-15 14:27:55 +0700

    ????

--- face_recog.py
+++ face_recog.py
@@ -4,14 +4,14 @@ import cv2
 
 cap = cv2.VideoCapture(0)  # default camera of laptop is 0
 
-KK_img = face_recognition.load_image_file('face-recognition\img_train\KK')
+KK_img = face_recognition.load_image_file('img_train\KK1.jpg')
 KK_face_encode = face_recognition.face_encodings(KK_img)[0]
 
-KK_img = face_recognition.load_image_file('face-recognition\img_train\KK')
-KK_face_encode = face_recognition.face_encodings(KK_img)[0]
+PLENG_img = face_recognition.load_image_file('img_train\PLENG1.jpg')
+PLENG_face_encode = face_recognition.face_encodings(PLENG_img)[0]
 
-KK_img = face_recognition.load_image_file('face-recognition\img_train\KK')
-KK_face_encode = face_recognition.face_encodings(KK_img)[0]
+WIN_img = face_recognition.load_image_file('img_train\WIN1.jpg')
+WIN_face_encode = face_recognition.face_encodings(WIN_img)[0]
 
 while True:
     ret, frame = cap.read()
