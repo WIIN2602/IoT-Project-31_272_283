48f68e21dbe12a08925c7e5a340824886410abd9
WIIN2602 <weeradech2602@gmail.com>
2024-03-15 19:52:02 +0700

    ????

--- face_recog.py
+++ face_recog.py
@@ -1,157 +1,25 @@
-import argparse
-import pickle
-from collections import Counter
-from pathlib import Path
+import cv2
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(r'haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+while True:
+    ret, img = cap.read()
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_img, 1.25, 4)
 
-import face_recognition
-from PIL import Image, ImageDraw
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        rec_gray = gray_img[y:y+h, x:x+w]
+        rec_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(rec_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(rec_color, (ex, ey),
+                          (ex + ew, ey + eh), (0, 127, 255), 2)
 
-DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
-BOUNDING_BOX_COLOR = "blue"
-TEXT_COLOR = "white"
+    cv2.imshow('Face Recognition', img)
 
-# Create directories if they don't already exist
-Path("training").mkdir(exist_ok=True)
-Path("output").mkdir(exist_ok=True)
-Path("validation").mkdir(exist_ok=True)
+    if cv2.waitKey(0) & 0xff == ord('q'):
+        break
 
-parser = argparse.ArgumentParser(description="Recognize faces in an image")
-parser.add_argument("--train", action="store_true", help="Train on input data")
-parser.add_argument(
-    "--validate", action="store_true", help="Validate trained model"
-)
-parser.add_argument(
-    "--test", action="store_true", help="Test the model with an unknown image"
-)
-parser.add_argument(
-    "-m",
-    action="store",
-    default="hog",
-    choices=["hog", "cnn"],
-    help="Which model to use for training: hog (CPU), cnn (GPU)",
-)
-parser.add_argument(
-    "-f", action="store", help="Path to an image with an unknown face"
-)
-args = parser.parse_args()
-
-
-def encode_known_faces(
-    model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
-) -> None:
-    """
-    Loads images in the training directory and builds a dictionary of their
-    names and encodings.
-    """
-    names = []
-    encodings = []
-
-    for filepath in Path("training").glob("*/*"):
-        name = filepath.parent.name
-        image = face_recognition.load_image_file(filepath)
-
-        face_locations = face_recognition.face_locations(image, model=model)
-        face_encodings = face_recognition.face_encodings(image, face_locations)
-
-        for encoding in face_encodings:
-            names.append(name)
-            encodings.append(encoding)
-
-    name_encodings = {"names": names, "encodings": encodings}
-    with encodings_location.open(mode="wb") as f:
-        pickle.dump(name_encodings, f)
-
-
-def recognize_faces(
-    image_location: str,
-    model: str = "hog",
-    encodings_location: Path = DEFAULT_ENCODINGS_PATH,
-) -> None:
-    """
-    Given an unknown image, get the locations and encodings of any faces and
-    compares them against the known encodings to find potential matches.
-    """
-    with encodings_location.open(mode="rb") as f:
-        loaded_encodings = pickle.load(f)
-
-    input_image = face_recognition.load_image_file(image_location)
-
-    input_face_locations = face_recognition.face_locations(
-        input_image, model=model
-    )
-    input_face_encodings = face_recognition.face_encodings(
-        input_image, input_face_locations
-    )
-
-    pillow_image = Image.fromarray(input_image)
-    draw = ImageDraw.Draw(pillow_image)
-
-    for bounding_box, unknown_encoding in zip(
-        input_face_locations, input_face_encodings
-    ):
-        name = _recognize_face(unknown_encoding, loaded_encodings)
-        if not name:
-            name = "Unknown"
-        _display_face(draw, bounding_box, name)
-
-    del draw
-    pillow_image.show()
-
-
-def _recognize_face(unknown_encoding, loaded_encodings):
-    """
-    Given an unknown encoding and all known encodings, find the known
-    encoding with the most matches.
-    """
-    boolean_matches = face_recognition.compare_faces(
-        loaded_encodings["encodings"], unknown_encoding
-    )
-    votes = Counter(
-        name
-        for match, name in zip(boolean_matches, loaded_encodings["names"])
-        if match
-    )
-    if votes:
-        return votes.most_common(1)[0][0]
-
-
-def _display_face(draw, bounding_box, name):
-    """
-    Draws bounding boxes around faces, a caption area, and text captions.
-    """
-    top, right, bottom, left = bounding_box
-    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
-    text_left, text_top, text_right, text_bottom = draw.textbbox(
-        (left, bottom), name
-    )
-    draw.rectangle(
-        ((text_left, text_top), (text_right, text_bottom)),
-        fill=BOUNDING_BOX_COLOR,
-        outline=BOUNDING_BOX_COLOR,
-    )
-    draw.text(
-        (text_left, text_top),
-        name,
-        fill=TEXT_COLOR,
-    )
-
-
-def validate(model: str = "hog"):
-    """
-    Runs recognize_faces on a set of images with known faces to validate
-    known encodings.
-    """
-    for filepath in Path("validation").rglob("*"):
-        if filepath.is_file():
-            recognize_faces(
-                image_location=str(filepath.absolute()), model=model
-            )
-
-
-if __name__ == "__main__":
-    if args.train:
-        encode_known_faces(model=args.m)
-    if args.validate:
-        validate(model=args.m)
-    if args.test:
-        recognize_faces(image_location=args.f, model=args.m)
+cap.release()
+cv2.destroyAllWindows()
